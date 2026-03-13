1eb833dea038b0d4848f9016adf62fdd88d5f981
evarandoAustral <eugenia.varando@ing.austral.edu.ar>
2026-03-13 12:56:22 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -14,11 +14,16 @@ def math():
     a = 57
     b = 7
 
-    print(a + b)
-    print(a - b)
-    print(a * b)
-    print((a + b) / 2)
-    print(a // b)
-    print(a % b)
-    print(a / b)
+    print(b+a)
+    print(a-b)
+    print(a*b)
+
+    print((a+b)/2)
+
+    print(a//b)
+    print(a%b)
+
+    print(a/b)
+
+
 
